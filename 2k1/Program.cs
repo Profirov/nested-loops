9442156af4019bb5e1177b27468df6481b26fc25
user <user@DESKTOP-Q7SRQOE>
2023-11-15 17:02:45 +0200

    Add project files.

---
+++ 2k1/Program.cs
@@ -0,0 +1,17 @@
+﻿//int n = int.Parse(Console.ReadLine());
+//int num = 1;
+
+//while (num <= n)
+//{
+//    Console.WriteLine(num);
+//    num = 2 * num + 1;
+//}
+
+
+int num = int.Parse(Console.ReadLine());
+while (num < 1 || num > 100)
+{
+    Console.WriteLine("Invalid number!");
+    num = int.Parse(Console.ReadLine());
+}
+Console.WriteLine($"The number is {num}.");
---
+++ stars/Program.cs
@@ -0,0 +1,12 @@
+﻿int  n = int.Parse(Console.ReadLine());
+
+for (int r = 1; r <= n; r++)
+{
+    Console.Write("*");
+    for (int c = 1; c < n; c++)
+    {
+        Console.WriteLine(" *");
+    }
+    Console.WriteLine();
+}
+
